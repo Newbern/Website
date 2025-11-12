06b3d791bf7036ab150b55a2220e6da83947a40f
steve <StevencNewbern@gmail.com>
2025-11-11 19:39:34 -0600

    New Project Image Update

--- app/backend/projects.php
+++ app/backend/projects.php
@@ -1,7 +1,7 @@
 <?php
 
-$companys = '[{"name": "Patricks Home Service", "url": "https://www.patrickshomeservice.com", "image": "app/media/Company/Patricks Home Service.png"},
-              {"name": "Bluff Creek Electric LLC", "url": "https://www.bluffcreekelectricllc.com", "image": "app/media/Company/Bluff Creek Electric.png"},
-              {"name": "Double Dubs", "url": "#projects", "image": "app/media/Company/Double Dubs.png"}]';
+$companys = '[{"name": "Southern Resturants", "url": "#projects", "image": "app/media/Figma/Southern Cooking.png"},
+              {"name": "Contactors", "url": "#projects", "image": "app/media/Figma/Contactors.png"},
+              {"name": "Advertizing", "url": "#projects", "image": "app/media/Figma/Advertizing.png"}]';
 
 ?>
--- app/templates/projects.php
+++ app/templates/projects.php
@@ -3,25 +3,17 @@
   <!-- Back button -->
   <button class="btn" onClick="change('Back')">&#8592;</button>
 
-  <!--Laptop Layout-->
-  <div class="laptop">
-    <!--Screen-->
-    <div class="screen">
-        <?php 
-        $comp = json_decode($companys);
-        foreach ($comp as $index => $company) {
-            if (!empty($company->name)) {
-              echo '<a id="'.$index.'" class="pages off" href="'.$company->url.'"><img src="'.$company->image.'" alt="'.$company->name.'"></a>';
-            }
-        }
-        ?>
-    </div>
-
-    <!--Base-->
-    <div class="base"></div>
+  <!-- Phone Layout-->
+  <div class="Phone">
+    <?php 
+      $comp = json_decode($companys);
+      foreach ($comp as $index => $company) {
+          if (!empty($company->name)) {
+            echo '<a id="'.$index.'" class="pages off" href="'.$company->url.'"><img src="'.$company->image.'" alt="'.$company->name.'"></a>';
+          }
+      }
+    ?>
   </div>
-
-  
 
   <!--Next Button-->
   <button class="btn" onClick="change('Next')">&#8594;</button>
